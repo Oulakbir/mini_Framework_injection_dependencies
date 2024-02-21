73255e5a6df8e6d0d593f336f0e003d632c896b4
OULAKBIR Ilham <86001779+Oulakbir@users.noreply.github.com>
2024-02-21 23:21:39 +0100

    commit3

--- src/main/java/ma/enset/miniprojet_jee/Inject.java
+++ src/main/java/ma/enset/miniprojet_jee/Inject.java
@@ -5,5 +5,18 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.METHOD})
 public @interface Inject {
+    /**
+     * Optional value specifying the name of the dependency to inject.
+     * @return the name of the dependency
+     */
+    String value() default "";
 
+    /**
+     * Optional flag indicating if the dependency is required.
+     * @return true if the dependency is required, false otherwise
+     */
+    boolean required() default true;
+
+    // Validation pour les éléments cibles appropriés
+    String[] allowedTargets() default { "FIELD", "CONSTRUCTOR", "METHOD" };
 }
